3b9e83798619aacc7779492390f619bdcdd7c9e8
AdamGuar <adam2hp@gmail.com>
2023-08-30 21:27:29 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import './app.css';
+
+function init(): void {
+    const button = document.querySelector("#testButton");
+    button?.addEventListener("click", testSubmitHandler);
+  }
+
+  function testSubmitHandler(e: Event): void {
+    e.preventDefault();
+    const outputElement = document.querySelector('#output');
+    outputElement.textContent = 'Scripts work!';
+  }
+
+  init();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'no-franeowrks-typescript-app-starter', 
+        template: 'src/index.html' }),
+        new MiniCssExtractPlugin()
+   ],
+
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
+};
